9f251cc4d67a84490d83ebe6d8fdaaaffa31cf4f
novak12 <novak12@163.com>
2018-11-09 19:05:46 +0800

    add notification button and pending activity

--- app/src/main/java/com/example/first/testdemo/MainActivity.java
+++ app/src/main/java/com/example/first/testdemo/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.first.testdemo;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -18,6 +22,7 @@ import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -186,5 +191,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent =new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent,CROP_PHOTO);
+    }
+
+    public void onClickNotification(Context context){
+        NotificationManager notificationManager = (NotificationManager) getSystemService
+                (NOTIFICATION_SERVICE);
+        //为了版本兼容  选择V7包下的NotificationCompat进行构造
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        //Ticker是状态栏显示的提示
+        builder.setTicker("简单Notification");
+        //第一行内容  通常作为通知栏标题
+        builder.setContentTitle("标题");
+        //第二行内容 通常是通知正文
+        builder.setContentText("通知内容");
+        //第三行内容 通常是内容摘要什么的 在低版本机器上不一定显示
+        builder.setSubText("这里显示的是通知第三行内容！");
+        //ContentInfo 在通知的右侧 时间的下面 用来展示一些其他信息
+        //builder.setContentInfo("3");
+        //number设计用来显示同种通知的数量和ContentInfo的位置一样，如果设置了ContentInfo则number会被隐藏
+        builder.setNumber(2);
+        //可以点击通知栏的删除按钮删除
+        builder.setAutoCancel(true);
+        //系统状态栏显示的小图标
+        builder.setSmallIcon(R.drawable.notify_5);
+        //下拉显示的大图标
+        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.launcher_sohu));
+        Intent intent = new Intent(context, PendingActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(context, 1, intent, 0);
+        //点击跳转的intent
+        builder.setContentIntent(pIntent);
+        //通知默认的声音 震动 呼吸灯
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        Notification notification = builder.build();
+        notificationManager.notify(TYPE_Normal, notification);
     }
 }
